0ffc671bd39c4083441ec250ca375b2e4c1a9b88
waguinhocm <waguinhocm@hotmail.com>
2025-04-14 09:45:11 -0300

    Javascript footer parcerias

--- parcerias/index.js
+++ parcerias/index.js
@@ -1,3 +1,6 @@
+
+//Main - captura e manipulação de dados do DOM
+
 arrayParceiros = [
     {
         nome: "Evino",
@@ -25,19 +28,17 @@ arrayParceiros = [
     },
 ];
 
+let main = document.querySelector("main");
+
 let titulo = document.createElement("h1");
 titulo.innerText = "Parcerias";
 
-let main = document.querySelector("main");
-
 main.appendChild(titulo);
 
-let div = document.createElement("div");
-
 for (let i = 0; i < arrayParceiros.length; i++) {
-    const parceiro = arrayParceiros[i];
+    let parceiro = arrayParceiros[i];
 
-    let div=document.createElement("div");
+    let div = document.createElement("div");
     div.classList.add("parceiro");
 
     div.innerHTML = `
@@ -47,5 +48,88 @@ for (let i = 0; i < arrayParceiros.length; i++) {
     main.appendChild(div);
 };
 
+//Footer - Logo
 
+let navFooter = document.querySelector("footer nav");
+let divLogoRodape = document.createElement("div");
+divLogoRodape.innerHTML = `
+<a href="../index.html"><img src="../imagens/Logo_pag.png" alt="Logotipo"height="42" width="52"></a>
+<h6>Copyright 2025 Clube do Vinho</h6>
+`;
+navFooter.appendChild(divLogoRodape);
+divLogoRodape.className = "rodape";
+navFooter.className = "rodape";
 
+//Footer - Navegação
+
+sectionNavRodape = document.createElement("section");
+sectionNavRodape.classList.add("navRodape");
+
+arrayNavRodape = [
+    {
+        nome: "Politicas de Privacidade",
+        link: "../politicasPrivacidade/index.html"
+    },
+
+    {
+        nome: "Contato",
+        link: "../contato/index.html"
+    },
+    {
+        nome: "Sobre nós",
+        link: "../sobreNos/index.html"
+    },
+];
+
+for (let i = 0; i < arrayNavRodape.length; i++) {
+    let nav = arrayNavRodape[i];
+
+    let section = document.createElement("section");
+    section.classList.add("navRodape");
+
+    section.innerHTML = `
+        <a href="${nav.link}">${nav.nome}</a>
+    `;
+    section.className = "navRodape";
+    sectionNavRodape.appendChild(section);
+};
+
+navFooter.appendChild(sectionNavRodape);
+
+//Footer - Redes Sociais
+
+let sectionRedes = document.createElement("section");
+sectionRedes.classList.add("redesSociais");
+
+let arrayRedesSociais = [
+    {
+        nome: "Instagram",
+        link: "https://www.instagram.com/",
+        img: "../imagens/Instagran.png"
+    },
+    {
+        nome: "Youtube",
+        link: "https://www.youtube.com/",
+        img: "../imagens/Youtube.png"
+    },
+    {
+        nome: "Facebook",
+        link: "https://www.facebook.com/",
+        img: "../imagens/Facebook.png"
+    },
+
+];
+
+for (let i = 0; i < arrayRedesSociais.length; i++) {
+    let redeSocial = arrayRedesSociais[i];
+
+    let div = document.createElement("div");
+    div.classList.add("redeSocial");
+
+    div.innerHTML = `
+        <a href="${redeSocial.link}" target="_blank"><img src="${redeSocial.img}" alt="logo ${redeSocial.nome}"height="30" width="30"></a>
+    `;
+    div.className = "redes";
+    sectionRedes.appendChild(div);
+};
+navFooter.appendChild(sectionRedes);
